0a3da6135d43d95a8a5c01d4597da6fb9ce8f080
Stephan Rabanser <steverab93@gmail.com>
2015-05-12 19:52:30 +0200

    Basic project setup

---
+++ TranzportMac/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  TranzportMac
+//
+//  Created by Stephan Rabanser on 12/05/15.
+//  Copyright (c) 2015 Stephan Rabanser. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
